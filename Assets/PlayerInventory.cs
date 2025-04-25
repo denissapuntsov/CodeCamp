c1b9f197addfa344acd4877e7e9c2a95d5bab519
Denis Sapuntsov <125272025+denissapuntsov@users.noreply.github.com>
2025-04-26 00:45:09 +0100

    Added Hat and GameObject Tilemap

--- Assets/PlayerInventory.cs
+++ Assets/PlayerInventory.cs
@@ -2,12 +2,24 @@ using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour
 {
+    [Header("Item Slots")]
     [SerializeField] private GameObject clothes;
-
+    
+    [Header("Transforms")]
+    [SerializeField] private Transform headGearParent;
+    
     public void PutOn(GameObject item)
     {
         if (clothes) return;
-        item.transform.SetParent(transform);
+        item.transform.SetParent(headGearParent, false);
+        item.GetComponent<Rigidbody>().isKinematic = true;
+        ResetTransform(item);
         clothes = item;
+    }
+
+    private void ResetTransform(GameObject obj)
+    {
+        obj.transform.localPosition = Vector3.zero;
+        obj.transform.localRotation = Quaternion.identity;
     }
 }
